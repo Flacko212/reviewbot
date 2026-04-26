9b26d99e36dcd8878d84d0c23dbb902c01271f32
Flacko212 <nathangale924@icloud.com>
2026-04-26 13:55:27 +0100

    server.py

--- server.py
+++ server.py
@@ -40,4 +40,6 @@ if __name__ == '__main__':
     print("  ReviewBot is running!")
     print("  Open your browser and go to: http://localhost:5000")
     print("")
-    app.run(debug=False, port=5000)
+    import os
+port = int(os.environ.get('PORT', 5000))
+app.run(debug=False, host='0.0.0.0', port=port)
